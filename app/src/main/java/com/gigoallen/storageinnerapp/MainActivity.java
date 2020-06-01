9a9f923ee849138a80bd4789d4ddc320c0053b28
gigoallen <gigo@hk.edu.tw>
2020-06-01 11:39:04 +0800

    save ok

--- app/src/main/java/com/gigoallen/storageinnerapp/MainActivity.java
+++ app/src/main/java/com/gigoallen/storageinnerapp/MainActivity.java
@@ -2,12 +2,44 @@ package com.gigoallen.storageinnerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String filename = "myNote.txt";
+    private static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void btnSave(View view) {
+
+        EditText edtInput = findViewById(R.id.edtInput);
+        String str = edtInput.getText().toString();
+
+        try {
+            FileOutputStream ops = openFileOutput(filename, MODE_PRIVATE);
+            OutputStreamWriter opw = new OutputStreamWriter(ops);
+            opw.write(str);
+            opw.flush();
+            opw.close();
+            Toast.makeText(this, "Save successfully", Toast.LENGTH_LONG).show();
+            edtInput.setText("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
